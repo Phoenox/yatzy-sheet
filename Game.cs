ca097257d0be13805bd9fc72f138dad4a0b47568
Thomas Bittner <thomas.bittner@posteo.de>
2021-06-06 11:08:08 +0200

    Allow to mark fields as crossed out

--- Game.cs
+++ Game.cs
@@ -22,20 +22,20 @@ namespace Yatzysheet
 
 	public class GameColumn
 	{
-		public int Ones { get; set; }
-		public int Twos { get; set; }
-		public int Threes { get; set; }
-		public int Fours { get; set; }
-		public int Fives { get; set; }
-		public int Sixes { get; set; }
+		public int? Ones { get; set; }
+		public int? Twos { get; set; }
+		public int? Threes { get; set; }
+		public int? Fours { get; set; }
+		public int? Fives { get; set; }
+		public int? Sixes { get; set; }
 
-		public int Triple { get; set; }
-		public int Quadruple { get; set; }
-		public int FullHouse { get; set; }
-		public int SmallStreet { get; set; }
-		public int BigStreet { get; set; }
-		public int Yatzy { get; set; }
-		public int Chance { get; set; }
+		public int? Triple { get; set; }
+		public int? Quadruple { get; set; }
+		public int? FullHouse { get; set; }
+		public int? SmallStreet { get; set; }
+		public int? BigStreet { get; set; }
+		public int? Yatzy { get; set; }
+		public int? Chance { get; set; }
 
 		public int ColumnSum
 		{
@@ -49,7 +49,17 @@ namespace Yatzysheet
 		}
 
 		public int TopSum {
-			get { return Ones + Twos + Threes + Fours + Fives + Sixes; }
+			get
+			{
+				var sum = 0;
+				sum += Ones ?? 0;
+				sum += Twos ?? 0;
+				sum += Threes ?? 0;
+				sum += Fours ?? 0;
+				sum += Fives ?? 0;
+				sum += Sixes ?? 0;
+				return sum;
+			}
 			set { }
 		}
 
@@ -59,7 +69,18 @@ namespace Yatzysheet
 		}
 
 		public int LowSum {
-			get { return Triple + Quadruple + FullHouse + SmallStreet + BigStreet + Yatzy + Chance; }
+			get
+			{
+				var sum = 0;
+				sum += Triple ?? 0;
+				sum += Quadruple ?? 0;
+				sum += FullHouse ?? 0;
+				sum += SmallStreet ?? 0;
+				sum += BigStreet ?? 0;
+				sum += Yatzy ?? 0;
+				sum += Chance ?? 0;
+				return sum;
+			}
 			set { }
 		}
 	}
